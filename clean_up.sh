6c0590a1f364f5c355e3cf0605c1d878098ae3bc
Andrey Kasatkin <a@svetliy.com>
2017-05-10 12:09:42 -0400

    add clean up script that will be processed during Zype publishing

---
+++ clean_up.sh
@@ -0,0 +1,10 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# remove things from the github repo for clean slate and reduce package size for Roku 1 compatibility
+rm -rf "$DIR/screenshots"
+rm "$DIR/README.md"
+find "$DIR" -name "*.DS_Store" -type f -delete
+rm -rf "$DIR/dist"
+rm -rf "$DIR/.git"
+rm "$DIR/.gitignore"
+rm -rf "$DIR/csfake"
